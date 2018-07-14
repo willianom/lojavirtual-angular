ff13eadd526982a700e13990ec53b5c298381cfb
willianom <willianom@gmail.com>
2018-07-14 08:37:36 -0400

    Incluindo dump do banco de dados

---
+++ lojavirtualtrs.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Community v12.4.0 (64 bit)
+MySQL - 5.6.26 : Database - lojavirtual_trs
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`lojavirtual_trs` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `lojavirtual_trs`;
+
+/*Table structure for table `produto` */
+
+DROP TABLE IF EXISTS `produto`;
+
+CREATE TABLE `produto` (
+  `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `descricao` varchar(200) NOT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  `imagem` mediumblob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `produto` */
+
+insert  into `produto`(`id`,`nome`,`descricao`,`preco`,`imagem`) values 
+(1,'Computador','Pentium1',100.00,NULL),
+(2,'Monitor','15 polegadas',500.00,NULL),
+(3,'Mouse','Optico',60.00,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
